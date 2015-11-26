c32b23535b2a9736d506e52738c7db902415a50c
Narender Gupta <ngupta18@illinois.edu>
2015-11-25 19:16:40 -0600

    Added basic experiment for hypotheis checking

--- shrink/src/experimenter.py
+++ shrink/src/experimenter.py
@@ -6,6 +6,8 @@ from itertools import combinations
 
 import csv
 import math
+import matplotlib.pyplot as plt
+import networkx as nx
 import os
 import statistics as stats
 
@@ -20,4 +22,53 @@ class Experimenter:
         self.dm = dm
         return None
 
+    
+    def get_accurate_result(self):
+        return None
 
+
+    def get_estimated_result(self, undergrad_grad, pgm_grad, att_dict):
+        undergrad = att_dict[U_UNIVERSITY_CODE]
+        grad = att_dict[UNIVERSITY]
+        pgm = att_dict[PROGRAM_CODE]
+        if undergrad_grad.has_edge(undergrad,grad) is False or \
+                pgm_grad.has_edge(pgm,grad) is False:
+                    return 0
+        else:
+            u_g_edges = undergrad_grad.edges()
+            out_undergrad_total = \
+                    sum([undergrad_grad[u][v]['weight'] \
+                    for u,v in u_g_edges if u==undergrad or v==undergrad])
+            out_undergrad_grad = undergrad_grad[undergrad][grad]['weight']
+            frac_1 = float(out_undergrad_grad) / out_undergrad_total
+
+            p_g_edges = pgm_grad.edges()
+            out_pgm_total = \
+                    sum([pgm_grad[u][v]['weight'] \
+                    for u,v in p_g_edges if u==pgm or v==pgm])
+            out_pgm_grad = pgm_grad[pgm][grad]['weight']
+            frac_2 = float(out_pgm_grad) / out_pgm_total
+            return frac_1 * frac_2
+
+
+    def get_grad_uni_summary_graph(self, attr):
+        G = nx.Graph()
+        uni_labels = self.dm.get_uni_labels()
+        for row in self.dm.data:
+            u_uni = row[attr]
+            if u_uni not in G:
+                G.add_node(u_uni)
+            for label in uni_labels:
+                if label in row and row[label] == ADMIT:
+                    if label not in G:
+                        G.add_node(label)
+                        G.add_edge(u_uni,label,weight=1)
+                    else:
+                        if (u_uni,label) not in G.edges():
+                            G.add_edge(u_uni,label,weight=1)
+                        else:
+                            G[u_uni][label]['weight'] += 1
+        #nx.draw_networkx(G,with_labels=True,)
+        #plt.savefig('../data/results/figures/summary_graph.png')
+        #plt.show()
+        return G
--- shrink/src/main.py
+++ shrink/src/main.py
@@ -3,18 +3,27 @@ from datamodel import DataModel
 from experimenter import Experimenter
 
 import argparse
-
+import time
 
 def main(args):
     dm = DataModel(args.data_file)
-    dm.read_data(to_read_count=2000,normalize_data=False)
+    dm.read_data(normalize_data=False)
     dm.set_data(
             dm.filter_data(
-                filter_type=LIST, feature=U_MAJOR_CODE, shortlist=['ee']))
-    print(len(dm.data))
+                filter_type=LIST, feature=U_MAJOR_CODE, shortlist=['ee','cs']))
     attributes_all = [U_GRADE_NORM, GRE_QUANT_NORM, GRE_VERBAL_NORM, GRE_AWA_NORM,\
             TOEFL_NORM, PROGRAM_CODE, U_MAJOR_CODE, TERM, YEAR, U_UNIVERSITY_RANK]
     exp = Experimenter(dm)
+    undergrad_to_grad_uni = exp.get_grad_uni_summary_graph(U_UNIVERSITY_CODE)
+    pgm_code_to_grad_uni = exp.get_grad_uni_summary_graph(PROGRAM_CODE)
+    print(len(undergrad_to_grad_uni.edges()), len(pgm_code_to_grad_uni.edges()))
+
+    att_dict = {U_UNIVERSITY_CODE:'www.unom.ac.in',\
+            PROGRAM_CODE:'ms',\
+            UNIVERSITY:'Arizona State University'}
+    est = exp.get_estimated_result(undergrad_to_grad_uni,
+            pgm_code_to_grad_uni, att_dict)
+    print(est)
     return None
 
 
